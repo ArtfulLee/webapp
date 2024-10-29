4fb34c586e543d5600e7120083e88a26c4155ab5
ArtfulLee <aastepsb@yandex.ru>
2024-10-29 19:45:53 +0300

    feat: add comments

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/**
+ * sap.ui.define - используется для определения глобального пространства имён в контроллерах и других модулях JavaScript.
+ * С помощью этого пространства имён объект может быть доступен во всём приложении.
+ * 
+ * "sap/ui/core/mvc/XMLView" - view SAPUI5 core ???
+ */
 sap.ui.define(["sap/ui/core/mvc/XMLView"], function (XMLView) {
   "use strict";
 
