c79c54b847d64e5a859ac0b011c1941118bc47c8
ArtfulLee <aastepsb@yandex.ru>
2024-11-16 15:21:29 +0300

    refactor: Step 9: Component Configuration

--- Component.js
+++ Component.js
@@ -6,29 +6,31 @@ sap.ui.define(
   ],
   function (UIComponent, JSONModel, ResourceModel) {
     "use strict";
+    
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
       metadata: {
-        "rootView": {
-          "viewName": "sap.ui.demo.walkthrough.view.App",
-          "type": "XML",
-          "async": true,
-          "id": "app",
+        rootView: {
+          viewName: "sap.ui.demo.walkthrough.view.App",
+          type: "XML",
+          async: true,
+          id: "app",
         },
       },
+
       init: function () {
         // call the init function of the parent
         UIComponent.prototype.init.apply(this, arguments);
         // set data model
-        var oData = {
+        let oData = {
           recipient: {
             name: "World",
           },
         };
-        var oModel = new JSONModel(oData);
+        let oModel = new JSONModel(oData);
         this.setModel(oModel);
 
         // set i18n model
-        var i18nModel = new ResourceModel({
+        let i18nModel = new ResourceModel({
           bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
         });
         this.setModel(i18nModel, "i18n");
