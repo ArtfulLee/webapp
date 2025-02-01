25e4a394cdef7e5757c212cf0dcada1e0bd82798
ArtfulLee <aastepsb@yandex.ru>
2025-02-01 15:57:25 +0300

    Step 32: Routing with Parameters

--- controller/InvoiceList.controller.js
+++ controller/InvoiceList.controller.js
@@ -36,8 +36,11 @@ sap.ui.define(
       },
 
       onPress: function (oEvent) {
+        let oItem = oEvent.getSource();
         let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-        oRouter.navTo("detail");
+        oRouter.navTo("detail", {
+          invoicePath: oItem.getBindingContext("invoice").getPath().substr(1),
+        });
       },
     });
   }
